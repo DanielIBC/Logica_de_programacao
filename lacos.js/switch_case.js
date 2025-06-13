c1ae901cd7fbf6527e53cb989affbbd6dec702ca
DanielIBC <danielsouza.souza249@gmail.com>
2025-06-13 11:32:10 -0400

    switch

---
+++ lacos.js/switch_case.js
@@ -0,0 +1,27 @@
+var dia = "quarta";
+
+switch (dia) {
+    case "segunda":
+        console.log("Dia de começar a semana");
+        break;
+
+    case "terça":
+        console.log("Hora de aprender coisas novas");
+        break;
+
+    case "quarta":
+        console.log("Metade da semana, continue firme");
+        break;
+    
+    case "quinta":
+        console.log("quase lá, mais um pouco de esforço");
+        break;
+    
+    case "sexta":
+        console.log("SEXTOOOU!");
+        break;
+    
+    default: 
+    console.log("Final de semana chegou");
+
+}
